4f99f9d8251b00719e9bd11f0d58cc3bea023e0d
ChinmayNoob <chinmaysk0408@gmail.com>
2025-07-12 12:02:55 +0530

    remove sidebar

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  "/collections(.)",
-  "/profile(.)",
+  "/collections(.*)",
+  "/profile(.*)",
   "/ask-question(.*)",
 ]);
 
@@ -12,7 +12,9 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).)",
+    "/(.*)",
     "/(api|trpc)(.*)",
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/(api|trpc)(.*)"
   ],
 };
